487034269a476fdd9f10abe0f28d928dd585fc47
Ajay Tripathi <ajay39in@gmail.com>
2018-02-08 10:38:05 +0530

    [Add] Information section

--- build/statics/js/main.js
+++ build/statics/js/main.js
@@ -1,5 +1,9 @@
-var skillsHtmlContainer = document.getElementById('skillsContainer');
-
+var html,
+    skillsHtmlContainer = document.getElementById('skillsContainer'),
+    introQuoteContainer = document.getElementById('introQuote'),
+    projectsContainer = document.getElementById('projectsContainer');
+document.body.scrollTop = document.documentElement.scrollTop = 0; // Scroll to Top of the page
+fetchData();
 function fetchData() {
     var metaDataList = [];
     var xhttp = new XMLHttpRequest();
@@ -9,29 +13,105 @@ function fetchData() {
         if (this.readyState == 4 && this.status == 200) {
             var response = new DOMParser().parseFromString(this.responseText, "text/html");
             metaDataList = response.evaluate("//div[@id='sheets-viewport']/div[@id='0']//table[@class='waffle']/tbody/tr/td//text()",response,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+            // Null the containers
+                skillsHtmlContainer.innerHTML = '';
             for (i=4;i<metaDataList.snapshotLength;i+=4)
                 htmlSkillBoxes(metaDataList.snapshotItem(i).nodeValue, metaDataList.snapshotItem(i+1).nodeValue, metaDataList.snapshotItem(i+2).nodeValue, metaDataList.snapshotItem(i+3).nodeValue);
+            metaDataList = response.evaluate("//div[@id='sheets-viewport']/div[@id='64538848']//table[@class='waffle']/tbody/tr/td//text()",response,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+            do {
+                random = Math.floor(Math.random() * metaDataList.snapshotLength);
+            } while(random < 3);
+            if(random % 2 == 0)
+                htmlQuotes(metaDataList.snapshotItem(random).nodeValue, metaDataList.snapshotItem(random+1).nodeValue);
+            else
+                htmlQuotes(metaDataList.snapshotItem(random-1).nodeValue, metaDataList.snapshotItem(random).nodeValue);
+            metaDataList = response.evaluate("//div[@id='sheets-viewport']/div[@id='1172547448']//table[@class='waffle']/tbody/tr/td//text()",response,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+            projectsContainer.innerHTML = '';
+            for (i=5;i<metaDataList.snapshotLength;i+=5)
+                htmlProjectBoxes(metaDataList.snapshotItem(i).nodeValue, metaDataList.snapshotItem(i+1).nodeValue, metaDataList.snapshotItem(i+2).nodeValue, metaDataList.snapshotItem(i+3).nodeValue, metaDataList.snapshotItem(i+4).nodeValue);
+
         }
     };
+}
+
+function htmlProjectBoxes(link, icon, title, body, longBody) {
+    if(window.innerWidth > 1100)
+        html = "<div id='projectBoxWrapper' class='col-4 col-12-sm'><div class='projectBox'><a href='" + link + "'><div class='projectBoxImage'><div class='projectBoxName'><i class='"+ icon +"'></i> "+ title +"</div></div></a><div class='projectBoxDesc'>"+ body +"</div><div class='projectBoxLongDesc'>" + longBody + "</div></div></div>";
+    else
+        html = "<div id='projectBoxWrapper' class='col-6 col-12-sm'><div class='projectBox'><a href='" + link + "'><div class='projectBoxImage'><div class='projectBoxName'><i class='"+ icon +"'></i> "+ title +"</div></div></a><div class='projectBoxDesc'>"+ body +"</div><div class='projectBoxLongDesc'>" + longBody + "</div></div></div>";
+    projectsContainer.innerHTML += html;
 }
 
 function htmlSkillBoxes(skillIcon, skillName, subSkillDesc, subSkillLevel) {
     subSkillDesc = subSkillDesc.split(", ");
     subSkillLevel = subSkillLevel.split(", ");
-    var html = "<div class='col-12-sm col-4'><div class='skillsContainerBox'><div class='sub-header-icon'><i class='"+skillIcon+"' aria-hidden='true'></i></div><div class='section-sub-header'>"+skillName+"</div><hr>";
+    html = "<div class='col-12-sm col-4'><div class='skillsContainerBox'><div class='sub-header-icon'><i class='"+skillIcon+"' aria-hidden='true'></i></div><div class='section-sub-header'>"+skillName+"</div><hr>";
     for(iterator in subSkillDesc) {
     if (subSkillLevel[iterator] == 'Comfortable')
-        html += "<div style='color: green' class='col-6'>"+subSkillDesc[iterator]+"</div><div class='col-6' style='color: green'>"+subSkillLevel[iterator]+"</div>";
+        html += "<div class='col-6  col-6-sm color-comfortable'>"+subSkillDesc[iterator]+"</div><div class='col-6  col-6-sm color-comfortable'>"+subSkillLevel[iterator]+"</div>";
     else if (subSkillLevel[iterator] == 'Intermediate')
-        html += "<div style='color: red' class='col-6'>"+subSkillDesc[iterator]+"</div><div class='col-6' style='color: red'>"+subSkillLevel[iterator]+"</div>";
+        html += "<div class='col-6  col-6-sm color-intermediate'>"+subSkillDesc[iterator]+"</div><div class='col-6  col-6-sm color-intermediate'>"+subSkillLevel[iterator]+"</div>";
     else if (subSkillLevel[iterator] == 'Learning')
-        html += "<div style='color: blue' class='col-6'>"+subSkillDesc[iterator]+"</div><div class='col-6' style='color: blue'>"+subSkillLevel[iterator]+"</div>";
+        html += "<div class='col-6  col-6-sm color-learning'>"+subSkillDesc[iterator]+"</div><div class='col-6  col-6-sm color-learning'>"+subSkillLevel[iterator]+"</div>";
     }
     html += "</div></div></div>";
     skillsHtmlContainer.innerHTML += html;
 }
 
-// toggleHometooltip();
-// void toggleHometooltip() {
-//     console.log("Nice Day Mate!");
-// }
+function htmlQuotes(introQuote, introSpeaker) {
+    introQuoteContainer.innerHTML = '';
+    if (introSpeaker != 'NULL')
+        html = '"'+introQuote+'"<br><div id="introSpeaker">— '+introSpeaker+'</div>';
+    else
+        html = '"'+introQuote+'"</div>';
+    introQuoteContainer.innerHTML += html;
+}
+
+showSnacks();
+function showSnacks() {
+    var snackbarContainer = document.getElementById("snackbar");
+    snackbarContainer.className = "show";
+    setTimeout(function(){ snackbarContainer.className = snackbarContainer.className.replace("show", ""); }, 3000);
+}
+
+function toggleSkillSectionSize(elementWrapper, elementToResize, boxSize) {
+    var sectionWrapper = document.getElementById(elementWrapper);
+    var buttonToResize = document.getElementById(elementToResize);
+    if (sectionWrapper.style.height != "100%") {
+        buttonToResize.innerHTML = 'Less';
+        sectionWrapper.style.height = "100%";
+    } else {
+        sectionWrapper.style.height = boxSize;
+        buttonToResize.innerHTML = 'More';
+    }
+}
+
+// <div class="item">
+//     <div class='expHolder'>
+//         <div class='row'><div class='expTitle col-11 col-11-sm'>Front-End Development</div></div>
+//         <div class='row'><p class='expBody col-11 col-11-sm'>I have worked on many front-end projects and i can comfortably work on <b> HTML, CSS, JavaScript, JQuery, KUTE.Js. </b></p></div>
+//         <div class='row'>
+//             <a class='col-5 col-5-sm expLink' title='Code: Exposure Project' href='https://github.com/ATB00ker/Exposure.github.io'>Exposure</a>
+//             <a class='col-5 col-5-sm expLink' title='Code: Portfolio' href='https://github.com/ATB00ker/portfolio'>Portfolio</a>
+//         </div>
+//     </div>
+// </div>
+// <div class="item">
+//     <div class='expHolder'>
+//         <div class='row'><div class='expTitle col-11 col-11-sm'>Node.Js</div></div>
+//         <div class='row'><p class='expBody col-11 col-11-sm'>I have worked on worked on Node.Js with <b>Express Framework</b> using <b> Pug </b> as the view engine.</p></div>
+//         <div class='row'>
+//             <a class='col-11 col-11-sm expLink' title='Code: Website on Node.Js' href='https://github.com/ATB00ker/theAmityAndroidClub'>Android Club</a>
+//         </div>
+//     </div>
+// </div>
+// <div class="item">
+//     <div class='expHolder'>
+//         <div class='row'><div class='expTitle col-11 col-11-sm'>C/C++</div></div>
+//         <div class='row'><p class='expBody col-11 col-11-sm'>I am comfortable & confident with using C/C++. I have done basic network management using C.</p></div>
+//         <div class='row'>
+//             <a class='col-11 col-11-sm expLink' title='Code: High School Final Project' href='https://github.com/ATB00ker/highSchoolProjectSample'>Bank Project</a>
+//         </div>
+//     </div>
+// </div>
+// </div>
